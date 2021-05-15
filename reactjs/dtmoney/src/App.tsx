5285065a299b34c2faea4245765194216e3a34ef
AndersonS7 <andersondez290@gmail.com>
2021-05-14 22:44:25 -0300

    finalizando a estilizaação do modal

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,14 +1,33 @@
-import styled from 'styled-components';
+import { useState } from 'react';
+import Modal from 'react-modal';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1 `
-  font-size: 64px;
-  color: #8257e6;
-`
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <Title>Hello world</Title>
-    </div>
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
   );
 }
